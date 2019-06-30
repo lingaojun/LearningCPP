9430f1c735c5fcaa3911c102beeb9dd2791d78e2
lingaojun <44394422+lingaojun@users.noreply.github.com>
2019-06-30 10:33:08 +0800

    Update
    增加了map和函数指针嵌套使用，实现了从map中寻找Key并对应查找到Value所在的函数指针，并使用它

--- 612/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ 612/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,6 +8,7 @@
 #include "algorithm"
 #include "string"
 #include "iterator"
+#include "Fun.h"
 namespace test1
 {
 	void output(int a, int b);
@@ -28,10 +29,9 @@ namespace test1
 #include "list"
 namespace test2
 {
-	std::vector<int> vec{12,4,43,5,436,345};
-	std::vector<int> vec2(vec);
-	std::list<char> lit1{ 'a','b','c','d','e' };
-	std::list<char> lit2{ 'b','c','d','e','f' };
+	std::vector<int> vec(4, 4);
+	std::list<char> lit1{ '1','2','3','4','5' };
+	std::list<char> lit2{ '3','4','5','6','7' };
 	std::list<int> lit3{ 13,42,54,12,56 };
 	void *output()
 	{
@@ -46,81 +46,65 @@ namespace test3
 	class Add //将类名字当成函数名字
 	{
 	public:
-		Add(int x) :sum(x) {};
 		int operator()(int a,int b)
 		{
-			sum += a + b;
-			return sum;
+			return a + b;
 		}
-	private:
-		int sum;
 	};
 	void fun(int a, int b, Add add)
 	{
 		std::cout << add(a, b) << std::endl;
 	}
 }
+
 namespace test4
 {
-	template<class T>
-	class My_ptr
+	MyStruct StrFun;
+	int (*fun[])(int &a, int &b) = 
 	{
-	public:
-		My_ptr(T *p) :_ptr(p) //C++类变量赋值新方法 在构造函数中使用":"可以对变量进行赋值。
-		{
-			std::cout << "生时获得资源" << std::endl;
-		}
-		T& operator *()
-		{
-			return *_ptr;
-		}
-		T* operator -> ()
-		{
-			return _ptr;
-		}
-		~My_ptr()
-		{
-			std::cout << "死前释放资源" << std::endl;
-			//delete _ptr; //delete会发生错误。 似乎是会触发二次释放内存，表示编译器已经释放一次内存了。
-		}
-	private:  
-		T *_ptr;
+		First,Second,Third
 	};
+	std::map<Enum, int*> maptest;
+	std::map<Enum, int (*)(int&,int&)> maptest2;
+	std::map<Enum, int(*)(int&, int&)>::iterator itrmap;
 }
 int main()
-{
+{	
 	std::cout << "Test2-----------------------------------------------------\n";
-	std::cout << "原Vector" << std::endl;
-	for_each(test2::vec.begin(), test2::vec.end(), [](int n) { std::cout << n <<std::endl; });//学习for_each
-	std::cout << "Vector进行降序排列" << std::endl;
-	sort(test2::vec.begin(), test2::vec.end(), std::greater<int>());
-	for_each(test2::vec.begin(), test2::vec.end(), [](int n) { std::cout << n << std::endl ; });//学习for_each
-	std::cout << "Vector进行升序排列" << std::endl;
-	sort(test2::vec.begin(), test2::vec.end()); //sort 第三个为适配器 默认为升序
-	for_each(test2::vec.begin(), test2::vec.end(), [](int n) { std::cout  << n  << std::endl; });//学习for_each
+	for_each(test2::vec.begin(), test2::vec.end(), [](int n) { std::cout << "for_each :n : " << n <<std::endl; });//按顺序输出vector中的内容
 	//for_each(test2::vec.begin(), test2::vec.end(), &test2::output);
-	copy(test2::lit3.begin(), test2::lit3.end(), std::ostream_iterator<int>(std::cout," "));//学习copy输出
+	copy(test2::lit3.begin(), test2::lit3.end(), std::ostream_iterator<int>(std::cout," "));//将list的内容按顺序以此复制到输出流中，相当于输出内容。
+	std::cout << std::endl << std::endl;//测试std::endl代表回车功能。
+	test2::lit3.sort();//测试list的排序功能，因为list内部为链表结构，地址顺序不连续，导致不能使用常规排序方式进行排序。
+	for_each(test2::lit3.begin(), test2::lit3.end(), [](int n) {std::cout << n << " "; });//同上
 	std::cout << std::endl << std::endl;
-	test2::lit3.sort();//学习list的sort排序
-	for_each(test2::lit3.begin(), test2::lit3.end(), [](int n) {std::cout << n << " "; });
-	std::cout << *find_first_of(test2::lit1.begin(), test2::lit1.end(), test2::lit2.begin(), test2::lit2.end()) << std::endl;//找到两个数组第一个相同的数值，并返回其值
+	std::cout << *find_first_of(test2::lit1.begin(), test2::lit1.end(), test2::lit2.begin(), test2::lit2.end()) << std::endl; //将两个链表中第一个相同的元素输出，因为函数返回值为第一个链表该元素的指针，需要加*
 	// std::cout << []() { return std::less<int>()(10, 12); } << std::endl;
 	std::cout << "Test1-----------------------------------------------------\n";
-	test1::test(1, 2, &test1::output);//学习函数指针
-	test1::test(3, 4, test1::ptr);
-	test1::test(5, 6, test1::ptr2);
+	test1::test(1, 2, &test1::output);//函数地址作为函数参数传入
+	test1::test(3, 4, test1::ptr);//同上
+	test1::test(5, 6, test1::ptr2);//同上
 	std::cout << test2::output << std::endl;
 	std::cout << "Test3-----------------------------------------------------\n";
-	test3::fun(1, 2, test3::Add(5)); //这里直接调用类的类型名字作为临时变量是允许的。
-	[](int x) {std::cout << "仿函数学习：" << x << std::endl; }(10); //学习仿函数
-	[](int x) ->int {std::cout << "仿函数学习："; std::cout << x << std::endl; return x; }(10);//仿函数返回值
-	std::cout << "Test4-----------------------------------------------------\n";
-	int a = 100;
-	{
-		test4::My_ptr<int> p = &a; //My_ptr<int>相当于int智能指针
-		std::cout << *p << std::endl;
-	}
-	std::cout << std::endl;
+	test3::fun(1, 2, test3::Add()); //这里直接调用类的类型名字作为临时变量是允许的。
+	std::cout << "Test4-----------------------------------------------------\n"; 
+	test4::StrFun.a = 1;
+	test4::StrFun.b = 2;
+	test4::StrFun.Fun = NULL;
+	test4::StrFun.Fun = First; //a+b 将函数地址变量直接赋值为另一个函数
+	std::cout << test4::StrFun.Fun(test4::StrFun.a, test4::StrFun.b) << std::endl;
+	test4::StrFun.Fun = Second; //a-b //同上
+	std::cout << test4::StrFun.Fun(test4::StrFun.a, test4::StrFun.b) << std::endl;
+	test4::StrFun.Fun = test4::fun[ThirdNum-1]; //a*b //将函数地址变量赋值为某函数返回值类型相同的数组中，达到了可以读取数组中表示的函数参数。
+	std::cout << test4::StrFun.Fun(test4::StrFun.a, test4::StrFun.b) << std::endl;
+	test4::maptest2[FirstNum] = First;
+	test4::maptest2[SecondNum] = Second;
+	test4::maptest2.insert(std::pair<Enum, int(*)(int&, int&)>(ThirdNum, Third));
+	test4::itrmap = test4::maptest2.find(FirstNum);
+	std::cout << test4::itrmap->second(test4::StrFun.a, test4::StrFun.b) << std::endl;
+	
+	
+
 	return 0;		
 }
 
